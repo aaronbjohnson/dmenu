8b675b93e9cca622c1192cb33a96fbfdaa96f7d9
Aaron Johnson <aaron@aaronjohnson.xyz>
2023-04-28 10:14:19 -0400

    april update

---
+++ config.h
@@ -0,0 +1,42 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* My Colors */
+static const char bright_red[] = "#ff2500";
+
+/* make new one-point-of-change-variable */
+static const char selected_text_color[] = "#77879B";
+
+/* Default Color Scheme */
+/*
+static const char *colors[SchemeLast][2] = {
+//	/*     fg         bg       */
+//	[SchemeNorm] = { "#bbbbbb", "#222222" },
+//	[SchemeSel] = { "#eeeeee", "#005577" },
+//	[SchemeOut] = { "#000000", "#00ffff" },
+//};
+
+
+/* My red color scheme */
+static const char *colors[SchemeLast][2] = {
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { selected_text_color, "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
