18926893536e696652b79937c119aeb73e74244b
aaron <aaron@aaronjohnson.xyz>
2025-08-21 13:57:07 -0400

    new

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"BerkeleyMono-Regular:size=22"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
@@ -17,7 +17,7 @@ static const char selected_text_color[] = "#ff2500";
 /* Default Color Scheme */
 /*
 static const char *colors[SchemeLast][2] = {
-//	/*     fg         bg       */
+//	*     fg         bg       */
 //	[SchemeNorm] = { "#bbbbbb", "#222222" },
 //	[SchemeSel] = { "#eeeeee", "#005577" },
 //	[SchemeOut] = { "#000000", "#00ffff" },
